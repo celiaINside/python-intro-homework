f69c53ec9e809bcfc34d45ad064775ebc9eb96af
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-15 17:59:10 -0400

    Revisions to warmup3.py

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -1,4 +1,4 @@
 # git log --oneline output:
-#4b2028c (HEAD -> assignment-2) committting assignment-2 file warmup2.py
+#7fd1cb2 committing warmup3.py
 
 print("This feels quite complicated but I've learned how a branch functions, how we move changes to the staging area to prep for committing, and then git commit them.")
