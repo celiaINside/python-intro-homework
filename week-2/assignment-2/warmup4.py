1d15e4872d3ac2ae706b92c513f13f569970e5c6
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-15 18:54:02 -0400

    Revising for clarity in error message

--- week-2/assignment-2/warmup4.py
+++ week-2/assignment-2/warmup4.py
@@ -3,5 +3,4 @@ print ("something here but i'm missing quotation marks")
 #Here is my buggy line of code: print (something here but i'm missing quotation marks")
 #The error message says: print (something here but i'm missing quotation marks")
                      #^SyntaxError: invalid syntax
-#The cause was the code was missing open quotation marks within the print function.
-#I'm adding them now to fix it but leaving the error in my code so that AIRHub can see it?
+#The cause of the error was the code was missing open quotation marks within the print function. I am adding those open quotation marks to fix it.
