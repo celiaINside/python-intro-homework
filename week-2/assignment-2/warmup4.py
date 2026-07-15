de0cb18b48c528dd23b4a01b8c566285d2f22850
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-15 18:33:14 -0400

    Revision - leave the bug in

--- week-2/assignment-2/warmup4.py
+++ week-2/assignment-2/warmup4.py
@@ -1,6 +1,8 @@
+print (something here but i'm missing quotation marks")
 print ("something here but i'm missing quotation marks")
 
 #Here is my buggy line of code: print (something here but i'm missing quotation marks")
-#The error message says: SyntaxError: invalid syntax
-#The cause was missing quotation marks within the print function.
-#I'm adding them now to fix it. 
+#The error message says: print (something here but i'm missing quotation marks")
+                     #^SyntaxError: invalid syntax
+#The cause was the code was missing open quotation marks within the print function.
+#I'm adding them now to fix it but leaving the error in my code so that AIRHub can see it?
