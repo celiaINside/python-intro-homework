59a572f6923db328fd83fdfc7183a4893633e389
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-15 18:24:56 -0400

    Revision again

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -2,3 +2,4 @@
 #7fd1cb2 committing warmup3.py
 
 print("This feels quite complicated but I've learned how a branch functions, how we move changes to the staging area to prep for committing, and then git commit them.")
+print ("I'm adding one more line of code here as a 'meaningful commit.'")
