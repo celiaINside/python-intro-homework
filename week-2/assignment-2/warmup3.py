86385535d56da13ae1709185e9181738750cc1bd
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-15 18:49:46 -0400

    Added a space

--- week-2/assignment-2/warmup3.py
+++ week-2/assignment-2/warmup3.py
@@ -1,5 +1,5 @@
 # git log --oneline output:
-#7fd1cb2 committing warmup3.py
+#59a572f (HEAD -> assignment-2, origin/assignment-2) Revision again
 
 print("This feels quite complicated but I've learned how a branch functions, how we move changes to the staging area to prep for committing, and then git commit them.")
 print ("I'm adding one more line of code here as a 'meaningful commit.'")
