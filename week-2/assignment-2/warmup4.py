db7ce02b1c600f2c77f6f3271991ffc929e92c74
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-15 15:01:51 -0400

    committing file or files?..

---
+++ week-2/assignment-2/warmup4.py
@@ -0,0 +1,5 @@
+print ("something here but i'm missing quotation marks")
+       
+#The error message says: SyntaxError: invalid syntax
+#The cause was missing quotation marks within the print function.
+#I'm adding them now to fix it. 
