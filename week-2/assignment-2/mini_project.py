bd12b7f5dee83375b357ab0879aa9e8cb200cecc
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-21 18:11:12 -0400

    added float function to my input function in line 1

--- week-2/assignment-2/mini_project.py
+++ week-2/assignment-2/mini_project.py
@@ -1,3 +1,3 @@
-temp_fahr = (input("Enter a temperature in Fahrenheit: "))
+temp_fahr = float(input("Enter a temperature in Fahrenheit: "))
 celsius = (temp_fahr - 32) * 5 / 9
 print (f"{temp_fahr}°F is {celsius:.1f}°C.")
