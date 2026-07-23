a2e9860a5986f0b59ff17e65d1f4901ad4b0ad99
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-23 13:29:19 -0400

    Added positive back to warmup4.

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -2,6 +2,8 @@ user_number = int(input("Enter a number: "))
 
 if user_number < 0:
     print (f"{user_number} is negative.")
+elif user_number > 0:
+    print (f"{user_number} is positive.")
 elif user_number == 0:
     print (f"{user_number} is zero.")
 
