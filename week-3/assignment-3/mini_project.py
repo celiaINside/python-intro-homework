2a271f9f0615489e3dc50ce8a505024ad991bd45
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-23 00:36:51 -0400

    Another revision on mini_project.py

--- week-3/assignment-3/mini_project.py
+++ week-3/assignment-3/mini_project.py
@@ -4,28 +4,28 @@ time = input("What time of day is it? ")
 day = day.strip().lower()
 time = time.strip().lower()
 
-if day != "monday" or day != "tuesday" or day != "wednesday" or day != "thursday" or day != "friday" or day != "saturday" or day != "sunday":
+if day != "monday" and day != "tuesday" and day != "wednesday" and day != "thursday" and day != "friday" and day != "saturday" and day != "sunday":
     print("Sorry, I don't recognize that day. Try: Monday, Tuesday, Wednesday...")
 elif time != "morning" and time != "afternoon" and time != "evening":
    print("Sorry, I don't recognize that time. Try: morning, afternoon, evening...")
 
-if day == "friday" or day == "monday" or day == "tuesday" and time == "morning":
+if day == "friday" and time == "morning":
     print(f"Suggestion: Morning lesson work with a cup of coffee!")
-if day == "friday" and time == "afternoon":
+elif day == "friday" and time == "afternoon":
     print(f"Suggestion: Afternoon lesson work with a cup of coffee!")
-if day == "friday" and time == "evening":
+elif day == "friday" and time == "evening":
     print(f"Suggestion: Go for a walk around the neighborhood!")
 
-if day == "saturday" or day == "wednesday" and time == "morning":
+if day == "saturday" and time == "morning":
     print(f"Suggestion: Morning lesson work with a cup of tea!")
-if day == "saturday" and time == "afternoon":
+elif day == "saturday" and time == "afternoon":
     print(f"Suggestion: Explore the city!")
-if day == "saturday" and time == "evening":
+elif day == "saturday" and time == "evening":
     print(f"Suggestion: Go out with friends tonight!")
 
-if day == "sunday" or day == "thursday" and time == "morning":
+if day == "sunday" and time == "morning":
     print(f"Suggestion: Read the news!")
-if day == "sunday" and time == "afternoon":
+elif day == "sunday" and time == "afternoon":
     print(f"Suggestion: Cook a great meal!")
-if day == "sunday" and time == "evening":
+elif day == "sunday" and time == "evening":
     print(f"Suggestion: Unwind with your favorite show!")
