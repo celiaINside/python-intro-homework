34168c5c39b29c63faf136e8dc3d21723a5bffaa
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-22 23:30:22 -0400

    Fixed a stray incorrect variable.

--- week-3/assignment-3/warmup2.py
+++ week-3/assignment-3/warmup2.py
@@ -6,7 +6,7 @@ elif age >= 13 and age <= 17:
     print ("You are a Teen.")
 elif age >= 18 and age <= 64:
     print ("You are an Adult.")
-elif score >= 80 and age <= 89:
+elif age >= 80 and age <= 89:
     print ("Grade: B")
 else:
     print ("You are a Senior.")
