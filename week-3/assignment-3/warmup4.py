4fd5a092f2b5d63db5796d9adb810a681a61de41
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-07-22 23:22:51 -0400

    Revised to form two separate code blocks.

--- week-3/assignment-3/warmup4.py
+++ week-3/assignment-3/warmup4.py
@@ -1,18 +1,16 @@
 user_number = int(input("Enter a number: "))
 print (f"Enter a number: {user_number}")
 
-if user_number < 0 and user_number % 2 == 0:
+if user_number < 0:
     print (f"{user_number} is negative.")
-    print (f"{user_number} is even.")
-elif user_number < 0 and user_number % 2 != 0:
-    print (f"{user_number} is negative.")
-    print (f"{user_number} is odd.")
-elif user_number > 0 and user_number % 2 == 0:
+elif user_number > 0:
     print (f"{user_number} is positive.")
+else:
+    print (f"{user_number} is zero.")
+
+if user_number % 2 == 0:
     print (f"{user_number} is even.")
-elif user_number > 0 and user_number % 2 != 0:
-    print (f"{user_number} is positive.")
+elif user_number % 2 != 0:
     print (f"{user_number} is odd.")
 else:
-    print (f"{user_number} is 0.")
     print (f"{user_number} is even.")
