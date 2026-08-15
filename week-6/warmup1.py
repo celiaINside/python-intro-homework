76b80c928dcc519863a805ad9cadb3669c0d218e
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-08-15 17:28:45 -0400

    First go on warmup1.

---
+++ week-6/warmup1.py
@@ -0,0 +1,14 @@
+#1
+
+def greet(name):
+    print("Hello, " + name + "!")
+
+#2
+def greet(name):
+    print("Good morning, " + name + "!")
+
+#3
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("Alex")
