58c5a91184fb1cd4d1e76743c366ecc25ee1c3da
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-08-16 22:45:39 -0400

    First go at warmup4

---
+++ week-6/assignment-6/warmup4.py
@@ -0,0 +1,12 @@
+def is_valid_score(score):
+    if 0 <= score <= 100:
+        return True
+    else: 
+        return False
+
+score = int(input("Enter a score: "))
+
+if is_valid_score(score):
+    print("Valid score.")
+else:
+    print("Invalid score — must be between 0 and 100.")
