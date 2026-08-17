4c9720cc1deb081f29970c41128b1f8833156e13
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-08-16 23:08:32 -0400

    Revising warmup4 code to make the function check if the input is valid instead of having the input code check straight away

--- week-6/assignment-6/warmup4.py
+++ week-6/assignment-6/warmup4.py
@@ -1,12 +1,13 @@
 def is_valid_score(score):
-    if isinstance(score, int) and 0 <= score <= 100:
-        return True
-    else: 
+    try:
+        score = int(score)
+        return 0 <= score <= 100
+    except ValueError:
         return False
 
-score = int(input("Enter a score: "))
+score = input("Enter a score: ")
 
-if is_valid_score(score):
+if is_valid_score(score) == True:
     print("Valid score.")
 else:
     print("Invalid score — must be between 0 and 100.")
