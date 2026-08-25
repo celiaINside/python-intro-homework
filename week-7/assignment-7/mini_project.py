53c726b0e8008b650c1b80a8ede109e1d7f78a5e
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-08-24 23:21:49 -0400

    Revision to add enumerate method to warmup1, and cleaned up code in miniproject

--- week-7/assignment-7/mini_project.py
+++ week-7/assignment-7/mini_project.py
@@ -17,13 +17,11 @@ with open('../data/expenses.csv', 'r') as file:
         item['amount'] = float(item['amount'])
 
 total_value = sum(item['amount'] for item in food)
-print(food)
-print(f"Food expenses sum: ${total_value:.2f}")
 
 from datetime import datetime
 now = datetime.now()
 with open('food_report.txt', 'w') as file:
-    file.write(f"Food Expense Report -- generated {now.strftime('%B %d, %Y')}\n")
+    file.write(f"Food Expense Report — generated {now.strftime('%B %d, %Y')}\n")
     for item in food:
         file.write(f"{item['date']}: ${item['amount']:.2f}\n")
-    file.write(f"Total: ${total_value:.2f}")
+    file.write(f"Total: ${total_value}")
--- week-7/assignment-7/warmup1.py
+++ week-7/assignment-7/warmup1.py
@@ -1,3 +1,7 @@
 with open('../data/notes.txt', 'r') as file:
-    for line in file:
-        print(line.strip())
+    for number, line in enumerate(file, start=1):
+        print(f"Line {number}: {line.strip()}")
+
+#for line in file:
+    #print(f"Line 1: {line.strip()}")
+    # Saving for posterity to see where I started.
