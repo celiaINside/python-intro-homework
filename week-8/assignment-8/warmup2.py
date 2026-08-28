9ce4badc62cefea55e1cdecc63a8426b8bc74e17
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-08-28 17:35:01 -0400

    Revised warmup2 to match output more closely

--- week-8/assignment-8/warmup2.py
+++ week-8/assignment-8/warmup2.py
@@ -5,6 +5,6 @@ try:
     input1 = float(input1)
     input2 = float(input2)
     result = (input1) / (input2)
-    print(f"{input1} / {input2} = {result:.1f}")
+    print(f"{input1} ÷ {input2} = {result:.1f}")
 except ZeroDivisionError:
     print("Can't divide by zero — please try a non-zero denominator.")
