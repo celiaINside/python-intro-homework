5166b073f1ad346dd77d5650422448f92a3ba01f
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-08-28 22:46:52 -0400

    Revise warmup2 to try converting to int instead...

--- week-8/assignment-8/warmup2.py
+++ week-8/assignment-8/warmup2.py
@@ -1,6 +1,9 @@
 input1 = input("Enter the numerator: ")
 input2 = input("Enter the denominator: ")
 
+input1 = int(input1)
+input2 = int(input2)
+
 try: 
     result = (input1) / (input2)
     print(f"{input1} ÷ {input2} = {result:.1f}")
