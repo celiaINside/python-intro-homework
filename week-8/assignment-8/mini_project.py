4b6ceb4fd40bb378cba6dc2ed61d3199b7155277
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-09-02 17:48:31 -0400

    Revised warmup2 to include a ValueError; revised mini_project to include needed code in the else block (it was outside before) .

--- week-8/assignment-8/mini_project.py
+++ week-8/assignment-8/mini_project.py
@@ -9,6 +9,7 @@ try:
         rows = list(reader)
 except FileNotFoundError:
     print('Error: "messy_data.csv" was not found. Please check the file path and try again.')
+
 else: 
     skipped_rows = []
     clean_rows = []
@@ -27,30 +28,29 @@ else:
 
             clean_rows.append(entry)
         except ValueError: 
-            value_error_message = f"  Row {row_number}: ValueError — could not convert '{row['amount']}' to float."
+            value_error_message = f"  Row {row_number}: ValueError — could not convert '{row['amount']}' to float"
             skipped_rows.append(value_error_message)
         except KeyError as e:
-            key_error_message = f"  Row {row_number}: missing column {e}"
+            key_error_message = f"  Row {row_number}: KeyError - could not find key {e}"
             skipped_rows.append(key_error_message)
-            continue
+            break
 
-skipped_total = len(skipped_rows)
-clean_total = len(clean_rows)
-attempted = len(rows)
-parsed = len(clean_rows)
+    skipped_total = len(skipped_rows)
+    attempted = len(rows)
+    parsed = len(clean_rows)
 
-print("=== CSV Report ===")
-print(f"Rows attempted: {attempted}")
-print(f"Rows parsed: {parsed}")
-print(f"Rows skipped: {skipped_total}")
-print()
-print("Skipped rows:")
-for row in skipped_rows: 
-    print(f" {row}")
-print()
-print(f"Clean data:")
-for row in clean_rows:
-    name = row['name']
-    category = row['category']
-    amount = row['amount']
-    print(f"  {name} | {category} | {amount}")
+    print("=== CSV Report ===")
+    print(f"Rows attempted: {attempted}")
+    print(f"Rows parsed: {parsed}")
+    print(f"Rows skipped: {skipped_total}")
+    print()
+    print("Skipped rows:")
+    for row in skipped_rows: 
+        print(f" {row}")
+    print()
+    print(f"Clean data:")
+    for row in clean_rows:
+        name = row['name']
+        category = row['category']
+        amount = row['amount']
+        print(f"  {name} | {category} | {amount}")
--- week-8/assignment-8/warmup2.py
+++ week-8/assignment-8/warmup2.py
@@ -6,7 +6,9 @@ input2 = input("Enter the denominator: ")
 try: 
     input1 = float(input1)
     input2 = float(input2)
-    result = (input1) / (input2)
+    result = input1 / input2
     print(f"{input1} ÷ {input2} = {result}")
+except ValueError:
+    print("Can't compute — please enter a valid integer.")
 except ZeroDivisionError:
     print("Can't divide by zero — please try a non-zero denominator.")
