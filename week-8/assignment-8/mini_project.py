94b57bc10810557f6e9ec87f31b0239aeec3fb7b
Jessica Bartlett <114193804+celiaINside@users.noreply.github.com>
2026-09-03 00:11:39 -0400

    Revised mini_project to make more concise and organized logic

--- week-8/assignment-8/mini_project.py
+++ week-8/assignment-8/mini_project.py
@@ -4,38 +4,43 @@ import csv
 try:
    with open("../data/messy_data.csv", "r") as file:
         reader = csv.DictReader(file)
-        rows = list(reader)
+
+        skipped_rows = []
+        clean_rows = []
+        attempted = 0
+
+        for row_number, row in enumerate(reader, start = 1):
+            attempted+= 1
+
+            if None in row:
+                none_message = f"Row {row_number}: extra column detected — skipped"
+                skipped_rows.append(none_message)
+                continue
+
+            try:
+                entry = {
+                    "name": row["name"],
+                    "category": row["category"],
+                    "amount": float(row["amount"])
+                }
+
+                clean_rows.append(entry)
+
+            except ValueError: 
+                value_error_message = f"Row {row_number}: ValueError — could not convert '{row['amount']}' to float"
+                skipped_rows.append(value_error_message)
+
+            except KeyError as e:
+                key_error_message = f"Row {row_number}: KeyError - could not find key {e}"
+                skipped_rows.append(key_error_message)
+
 except FileNotFoundError:
     print('Error: "messy_data.csv" was not found. Please check the file path.')
 
-else: 
-    skipped_rows = []
-    clean_rows = []
-    for row_number, row in enumerate(rows, start = 1):
-        if None in row:
-            none_message = f"  Row {row_number}: extra column detected — skipped"
-            skipped_rows.append(none_message)
-            continue
-
-        try:
-            entry = {
-                "name": row["name"],
-                "category": row["category"],
-                "amount": float(row["amount"])
-            }
-
-            clean_rows.append(entry)
-        except ValueError: 
-            value_error_message = f"  Row {row_number}: ValueError — could not convert '{row['amount']}' to float"
-            skipped_rows.append(value_error_message)
-        except KeyError as e:
-            key_error_message = f"  Row {row_number}: KeyError - could not find key {e}"
-            skipped_rows.append(key_error_message)
-
+else:
     skipped_total = len(skipped_rows)
-    attempted = len(rows)
+    attempted
     parsed = len(clean_rows)
-
     print("=== CSV Report ===")
     print(f"Rows attempted:  {attempted}")
     print(f"Rows parsed:      {parsed}")
@@ -43,7 +48,7 @@ else:
     print()
     print("Skipped rows:")
     for row in skipped_rows: 
-        print(f"{row}")
+        print(f"  {row}")
     print()
     print(f"Clean data:")
     for row in clean_rows:
